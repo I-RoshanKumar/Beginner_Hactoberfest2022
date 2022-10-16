61045985652b7ef61961a3ed0c65fc6f1d965012
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2022-10-16 12:08:07 +0530

    Solved the #22 issue

---
+++ Python/AGE.py
@@ -0,0 +1,7 @@
+# Write a program to check whether a person is elector or not
+age=(int( input("Enter the age of the person :")))
+if age>=18:
+    print("The elector is VALID")
+else:
+    print("NOT VALID AGE")
+    
