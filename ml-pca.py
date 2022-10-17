1255d644c0bd48de1e1df60ac8385a6c8d094a0d
doldrums <doldrums.317@gmail.com>
2022-10-18 01:41:41 +0300

    comments

--- ml-pca.py
+++ ml-pca.py
@@ -1,3 +1,9 @@
+# make sure you installed
+# pip3 install sklearn
+# pip3 install numpy
+# pip3 install pandas
+# pip3 install matplotlib
+
 from sklearn.datasets import load_breast_cancer
 import matplotlib.pyplot as plt
 import numpy as np
