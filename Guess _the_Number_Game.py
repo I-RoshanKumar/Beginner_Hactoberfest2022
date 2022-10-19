bcccd892ad34c1f6b19cb5e3993e2276deccf18f
Amaan ansari <86655270+amaan0810@users.noreply.github.com>
2022-10-19 11:45:22 +0530

    Added Guess the Number Game using python

---
+++ Guess _the_Number_Game.py
@@ -0,0 +1,29 @@
+import random
+
+guessesTaken = 0
+
+print('Hello! What is your name?' )
+myName = input()
+
+number = random.randint(1, 100)
+print('Well, ' + myName + ' I am thinking of a number between 1 and 100')
+
+while guessesTaken < 6:
+    print('Take a guess. ') 
+    guess = input()
+    guess = int(guess)
+    guessesTaken = guessesTaken + 1
+    if guess < number:
+        print('Your guess is too low.') 
+    if guess > number:
+        print('Your guess is too high.')
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print('Good job, ' + myName + ' You guessed ny number in ' + guessesTaken + ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number)
